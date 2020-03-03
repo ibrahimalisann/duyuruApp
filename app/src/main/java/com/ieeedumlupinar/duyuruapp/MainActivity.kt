1f8060b6ac325e38c110b4abeddbe08c36bd19a6
Zafer Çalışkan <caliskan.zafer@outlook.com>
2020-03-03 22:07:23 +0300

    İlk değişiklik

--- app/src/main/java/com/ieeedumlupinar/duyuruapp/MainActivity.kt
+++ app/src/main/java/com/ieeedumlupinar/duyuruapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ieeedumlupinar.duyuruapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = "Hasan selam"
     }
 }
